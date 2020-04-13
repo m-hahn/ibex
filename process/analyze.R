2abd0eaea01ffbf8fe10c6687301a6e29d27aa26
Michael Hahn <mhahn29@gmail.com>
2020-04-13 21:13:34 +0800

    code

--- process/analyze.R
+++ process/analyze.R
@@ -14,13 +14,22 @@ trials = merge(regions, trials, by=c("Group", "WordNumber", "Type"))
 mean(trials$Word == trials$WordFromItem | trials$WordFromItem == "NOUN")
 
 
-critical = trials %>% filter(Region == "v1") %>% group_by(Group, Type, Participant) %>% summarise(ReadingTimeToFirstAnswer = sum(ReadingTimeToFirstAnswer)) %>% mutate(LogTime = log(ReadingTimeToFirstAnswer))
+
+
+nouns = trials %>% filter(WordFromItem == "NOUN") %>% select(Participant, Group, Type, Word) %>% rename(Noun = Word)
+
+trials = merge(trials, nouns, by=c("Participant", "Group", "Type"), all=TRUE)
+
+
+critical = trials %>% filter(Region == "v1") %>% group_by(Group, Type, Participant, Noun) %>% summarise(ReadingTimeToFirstAnswer = sum(ReadingTimeToFirstAnswer)) %>% mutate(LogTime = log(ReadingTimeToFirstAnswer))
 
 summary(lmer(LogTime ~ Type + (1+Type|Group) + (1+Type|Participant), data=critical))
 
-critical = trials %>% filter(Region == "v1") %>% group_by(Group, Type, Participant) %>% filter(NumberInRegion == 1) %>% mutate(LogTime = log(ReadingTimeToFirstAnswer))
+critical = trials %>% filter(Region == "v1") %>% group_by(Group, Type, Participant, Noun) %>% filter(NumberInRegion == 1) %>% mutate(LogTime = log(ReadingTimeToFirstAnswer))
 
-summary(lmer(LogTime ~ Type + (1+Type|Group) + (1+Type|Participant), data=critical))
+summary(lmer(LogTime ~ Type + (1|Noun) + (1+Type|Group) + (1+Type|Participant), data=critical))
+
+
 
 
 errors = trials %>% filter(ErrorHere) %>% select(Group, Type, Participant, WordNumber, Word)
